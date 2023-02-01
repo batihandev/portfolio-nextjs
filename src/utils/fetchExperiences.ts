37de89584267bc902f9ee78bb999d2b470893eed
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-02-01 17:25:06 +0300

    testing

--- src/utils/fetchExperiences.ts
+++ src/utils/fetchExperiences.ts
@@ -1,13 +1,19 @@
 import { Experience } from "typings";
 
 export const fetchExperiences = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`,
-    { next: { revalidate: 1000 } }
-  );
-  const data = await res.json();
-  const experiences: Experience[] = data.experiences;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`,
+      { next: { revalidate: 1000 } }
+    );
+    const data = await res.json();
+    const experiences: Experience[] = data.experiences;
 
-  //   console
-  return experiences;
+    //   console
+    return experiences;
+  } catch (e) {
+    console.log(e);
+    const experiences = "error";
+    return experiences;
+  }
 };
--- src/utils/fetchPageInfo.ts
+++ src/utils/fetchPageInfo.ts
@@ -1,13 +1,19 @@
 import { PageInfo } from "typings";
 
 export const fetchPageInfo = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`,
-    { next: { revalidate: 1000 } }
-  );
-  const data = await res.json();
-  const pageInfo: PageInfo = data.pageInfo;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`,
+      { next: { revalidate: 1000 } }
+    );
+    const data = await res.json();
+    const pageInfo: PageInfo = data.pageInfo;
 
-  //   console
-  return pageInfo;
+    //   console
+    return pageInfo;
+  } catch (e) {
+    console.log(e);
+    const pageInfo = "Error";
+    return pageInfo;
+  }
 };
