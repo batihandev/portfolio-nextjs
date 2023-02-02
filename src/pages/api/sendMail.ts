bb329a4639f650a500ef047f50ed1875239cfc9a
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-02-03 02:14:51 +0300

    email fixes

--- src/pages/api/sendMail.ts
+++ src/pages/api/sendMail.ts
@@ -12,9 +12,39 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<boolean>
 ) {
+  async function mail({ name, email, subject, message }: Data) {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        type: "OAuth2",
+        user: "batihanportfolio@gmail.com",
+        clientId:
+          "345305089409-eitp0oudde6mpbgsqbb6us1bejse2jn5.apps.googleusercontent.com",
+        clientSecret: "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",
+        refreshToken:
+          "1//04jWrGI7pvOFqCgYIARAAGAQSNwF-L9IrPZl92fGGGlb4nr0t4GaWwVaZ1jcPeHgJdFIAKl35V-4UaxATFiz6B16BVpOyg2Z3a5o",
+        accessToken:
+          "ya29.a0AVvZVspBYP0hHrpdweDDtM0nzUoUgAHP0o7TVOTXjeUhKSKNq5baPnoFw4t9qFLyzbv3fYJsoXJmQl7oLkOs_06RmcCek4vXW-nQ24k4gAtUvkiQsQX1c1CKzTUi1uYjHADTa4hFAQSEd9YkC_qwbF84_GNDaCgYKAWcSARMSFQGbdwaIfAhbzIm2HlflSyAkXw2p1A0163",
+      },
+    });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Portfoliom Contact" <batihanportfolio@gmail.com>', // sender address
+      to: "batihanportfolio@gmail.com", // list of receivers
+      subject: `${subject}`, // Subject line
+      text: `${message} email:${email} name:${name}`, // plain text body
+      html: `<b>email: ${email} name: ${name}</b> <p>${message}</>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    res.status(200).json(true);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  }
+
   try {
     mail(JSON.parse(req.body));
-    res.status(200).json(true);
   } catch {
     res.status(400).json(false);
   }
@@ -25,31 +55,3 @@ export {};
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
-async function mail({ name, email, subject, message }: Data) {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      type: "OAuth2",
-      user: "batihanportfolio@gmail.com",
-      clientId:
-        "345305089409-eitp0oudde6mpbgsqbb6us1bejse2jn5.apps.googleusercontent.com",
-      clientSecret: "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",
-      refreshToken:
-        "1//04jWrGI7pvOFqCgYIARAAGAQSNwF-L9IrPZl92fGGGlb4nr0t4GaWwVaZ1jcPeHgJdFIAKl35V-4UaxATFiz6B16BVpOyg2Z3a5o",
-      accessToken:
-        "ya29.a0AVvZVspBYP0hHrpdweDDtM0nzUoUgAHP0o7TVOTXjeUhKSKNq5baPnoFw4t9qFLyzbv3fYJsoXJmQl7oLkOs_06RmcCek4vXW-nQ24k4gAtUvkiQsQX1c1CKzTUi1uYjHADTa4hFAQSEd9YkC_qwbF84_GNDaCgYKAWcSARMSFQGbdwaIfAhbzIm2HlflSyAkXw2p1A0163",
-    },
-  });
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Portfoliom Contact" <batihanportfolio@gmail.com>', // sender address
-    to: "batihanportfolio@gmail.com", // list of receivers
-    subject: `${subject}`, // Subject line
-    text: `${message} email:${email} name:${name}`, // plain text body
-    html: `<b>email: ${email} name: ${name}</b> <p>${message}</>`, // html body
-  });
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-}
