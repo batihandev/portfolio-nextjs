cde4e5a7a1f4c169b18f4facc4a05da59e2f39ad
Batihan Ozdemir <batihandev@gmail.com>
2024-07-09 20:11:05 +0300

    some extra metadata seo improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
-
+import Script from "next/script";
+import { Person, WithContext } from "schema-dts";
+import Head from "next/head";
 export const metadata: Metadata = {
   title: "Batıhan Özdemir",
   description: "Batıhan Özdemir's Portfolio @batihandev",
+  keywords:
+    "Batıhan Özdemir, Web Developer, Web Designer, Portfolio, @batihandev, Next.js, React, TailwindCSS, Nuxt.js, Vue, Node.js",
 };
 
 export default function RootLayout({
@@ -11,9 +15,29 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const jsonLd: WithContext<Person> = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Batıhan Özdemir",
+    url: "https://www.batihan.dev",
+    description: "Batıhan Özdemir's Portfolio @batihandev",
+    sameAs: [
+      "https://www.x.com/batihandev",
+      "https://www.github.com/batihandev",
+      "https://www.linkedin.com/in/batihandev",
+    ],
+  };
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
+
       <body>{children}</body>
     </html>
   );
