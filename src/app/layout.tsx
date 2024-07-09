f06d98ca19df13f23798887fb3fad0eef9bf56f5
Batihan Ozdemir <batihandev@gmail.com>
2024-07-09 19:43:23 +0300

    favicon in metadata fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Batıhan Özdemir",
   description: "Batıhan Özdemir's Portfolio @batihandev",
-  favicon: "/favicon.ico",
 };
 
 export default function RootLayout({
