d7dea03368c1def93b81962d0fec0b72add4827b
Batihan Ozdemir <batihandev@gmail.com>
2024-07-09 19:37:25 +0300

    metadata changes for my portfolio

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,10 @@ export default function Head() {
     <>
       <title>Batıhan Özdemir</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Batıhan Özdemir's Portfolio" />
+      <meta
+        name="description"
+        content="Batıhan Özdemir's Portfolio @batihandev"
+      />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
