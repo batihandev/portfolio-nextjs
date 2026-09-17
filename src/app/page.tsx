113892a1da793a1898eb45b549d6939c796b8802
batihandev <batihandev@gmail.com>
2026-09-17 20:02:40 +0300

    copy: projects intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default async function HomePage() {
         <Section
           id="projects"
           title="Things I'm building"
-          intro="Games and apps I make in my own time, most of them to solve something I wanted myself."
+          intro="Games, apps and tools I build outside work."
         >
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {projects.map((project, i) => (
